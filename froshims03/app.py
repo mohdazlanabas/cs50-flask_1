3ecb22c8397f79e04ec9eb6dfc0c67cbbd93c605
Azlan Abas <mohdazlan.abas@gmail.com>
2022-09-23 10:55:04 +0800

    Initial Commit

---
+++ froshims03/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from cs50 import SQL
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///froshims.db")
+
+SPORTS = [
+    "Basketball", "Soccer", "Hockey", "Football", "Frisbee"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route("/deregister", methods=["POST"])
+def deregister():
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+    return redirect("/registrants")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    if not name or sport not in SPORTS:
+        return render_template("failure.html")
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?,?)", name, sport)
+    return redirect("/registrants")
+
+@app.route("/registrants")
+def registrants():
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
